be3167cc415ef673e2931cc58654acddabb04c35
shabeeb483 <sshabeeb03@gmail.com>
2025-12-31 11:08:33 +0530

    test removrd

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ elif st.session_state.step == 3:
         <h1>🎟 GOLDEN LOVE TICKET 🎟</h1>
         <h3>Issued Only For You 💖</h3>
         <hr>
-        <h2>Unlimited test Love</h2>
+        <h2>Unlimited  Love</h2>
         <h2>Unlimited Smiles</h2>
         <p>Valid Forever ♾</p>   
     </div> 
